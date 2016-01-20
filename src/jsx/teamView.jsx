438804010c6b6d466e2188a557eabbea70ebbbb8
Akhil Palla <akhil@theakhil.com>
2016-01-19 20:55:00 -0800

    Add more to teamView.jsx

--- src/jsx/teamView.jsx
+++ src/jsx/teamView.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
+
+var Firebase = require("firebase");
+
+var firebaseRef = new Firebase("https://teamdata.firebaseio.com/");
+var teambase;
+var matchbase;
+
 module.exports = React.createClass({
+
+  getInitialState: function() {
+    return {
+        location: "locationboi",
+        name: "teamname",
+        nick: "nickname",
+        number: "number",
+        website: "website"
+    };
+  },
 
   render: function() {
     return (
       <div>
         <h3>Team {this.props.params.id}</h3>
+        <h5>{this.state.location}</h5>
+        <h5>{this.state.name}</h5>
+        <h5>{this.state.nick}</h5>
+        <h5>{this.state.number}</h5>
+        <h5>{this.state.website}</h5>
       </div>
     );
   },
 
-  componentDidMount() {
-   const id = this.props.params.id
+  componentDidMount(){
+      teambase = firebaseRef.child(`teams/frc${this.props.params.id}`);
+      matchbase = firebaseRef.child(`/matches`);
+      matchbase.orderByChild("team").equalTo(this.props.params.id).once("value", function(dataSnapshot){
+          console.log(dataSnapshot.val());
+      });
+      teambase.on("value", function(dataSnapshot) {
+        var data = dataSnapshot.val();
+        this.setState({
+            location: data.location,
+            name: data.name,
+            nick: data.nick,
+            number: data.number,
+            website: data.website
+        });
+      }.bind(this));
   }
+
+
 
 });
